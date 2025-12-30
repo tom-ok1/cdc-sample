b19215f6ada767af7d8c7410b5e5030b3baba394
tom-ok1 <tom.ok1@icloud.com>
2025-12-30 16:54:07 +0900

    refactor: update DebeziumEnvelope class and register-connector.json configuration; improve Docker Compose formatting

--- consumer/src/main/java/com/example/consumer/dto/DebeziumEnvelope.java
+++ consumer/src/main/java/com/example/consumer/dto/DebeziumEnvelope.java
@@ -1,9 +1,11 @@
 package com.example.consumer.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DebeziumEnvelope {
     private JsonNode before;
     private JsonNode after;
